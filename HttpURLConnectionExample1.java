9078b716d849ef8b090b4f96b30101e3c7df45bb
Alok Anand <32493130+alokanand18@users.noreply.github.com>
2020-05-04 18:33:52 +0530

    Add files via upload

---
+++ HttpURLConnectionExample1.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class HttpURLConnectionExample1 {
+
+
+	private static final String POST_URL = "https://api.boomi.com/api/rest/v1/trainingalokanand-4TKUEM/DeployedPackage/";
+
+	private static final String POST_PARAMS = "{\n" + "\"environmentId\" : \"		e48c5198-b700-49af-b4e4-8860af313c4f\",\r\n" + "\"packageId\" : \"5a9161ac-b18e-4a41-b940-18d35693952b\",\r\n" +  "\"notes\" : \"Package deployment via Jenkins Test user\"" + "\n}";
+
+	public static void main(String[] args) throws IOException {
+
+		
+		sendPOST();
+		System.out.println("POST DONE");
+	}
+
+	private static void sendPOST() throws IOException {
+		URL obj = new URL(POST_URL);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("Content-Type", "application/json");
+		con.setRequestProperty("Authorization", "Basic YWxvay5hbmFuZEBuZW9zYWxwaGEuY29tOmRlbGwxMjM0");
+
+		// For POST only - START
+		con.setDoOutput(true);
+		OutputStream os = con.getOutputStream();
+		os.write(POST_PARAMS.getBytes());
+		os.flush();
+		os.close();
+		// For POST only - END
+
+		int responseCode = con.getResponseCode();
+		System.out.println("POST Response Code :: " + responseCode);
+
+		if (responseCode == HttpURLConnection.HTTP_OK) { //success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			System.out.println(response.toString());
+		} else {
+			System.out.println("POST request not worked");
+		}
+	}
+
+}
