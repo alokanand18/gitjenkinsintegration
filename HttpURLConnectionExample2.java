dc9d3cf5efb65f8155812c1df8dfb04e898a122f
Alok Anand <32493130+alokanand18@users.noreply.github.com>
2020-05-08 14:36:02 +0530

    Update HttpURLConnectionExample2.java

--- HttpURLConnectionExample2.java
+++ HttpURLConnectionExample2.java
@@ -13,7 +13,7 @@ public class HttpURLConnectionExample2 {
 
 	private static final String POST_URL = "https://api.boomi.com/api/rest/v1/trainingalokanand-4TKUEM/DeployedPackage/";
 
-	private static final String POST_PARAMS = "{\n" + "\"environmentId\" : \"3b2f9079-c5d2-4fd1-9cc7-dcd0cbfda164\",\r\n" + "\"packageId\" : \"7d3a2fcd-9b4f-4648-980a-c37e3d932e93\",\r\n" +  "\"notes\" : \"Package deployment via Jenkins Dev user\"" + "\n}";
+	private static final String POST_PARAMS = "{\n" + "\"environmentId\" : \"3b2f9079-c5d2-4fd1-9cc7-dcd0cbfda164\",\r\n" + "\"packageId\" : \"6d464414-8ec1-43ac-9657-d59a7897a3a1\",\r\n" +  "\"notes\" : \"Package deployment via Jenkins Dev user\"" + "\n}";
 
 	public static void main(String[] args) throws IOException {
 
